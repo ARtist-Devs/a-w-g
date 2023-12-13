697410285d4813719fccd66d0ec264dd415e0beb
Yönet <3523671+Yonet@users.noreply.github.com>
2023-12-13 18:33:31 +0300

    Before update

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,37 +9,38 @@ import {
 	signal,
 } from "@angular/core";
 
-import { Artwork } from "projects/three/src/lib/artwork";
 import {
+	Artwork,
 	ArtworkFramesService,
 	LoadersService,
 	SceneService,
 	UIService,
 } from "projects/three/src/public-api";
+
 import { ArtworksService } from "../artworks.service";
 
-@Component({
+@Component( {
 	selector: "art-gallery",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: "./gallery.component.html",
 	styleUrls: ["./gallery.component.scss"],
-})
+} )
 export class GalleryComponent implements OnInit {
 	public loadingBar = true;
 	public loadingClass = "loading";
-	public loadingProgress: WritableSignal<number> = signal(0);
+	public loadingProgress: WritableSignal<number> = signal( 0 );
 	public frames: any;
+
 	private artworks: Artwork[] = [];
 	private artworksLength = 0;
-
 	private buttons: any[];
 	private focused = 0;
-
 	private selectedArtwork: WritableSignal<Artwork>;
-	@ViewChild("canvas", { static: true })
+
+	@ViewChild( "canvas", { static: true } )
 	canvas!: ElementRef<HTMLCanvasElement>;
 
-	private get canvasEl(): HTMLCanvasElement {
+	private get canvasEl (): HTMLCanvasElement {
 		return this.canvas?.nativeElement;
 	}
 
@@ -49,34 +50,34 @@ export class GalleryComponent implements OnInit {
 		private artworksService: ArtworksService,
 		private framesService: ArtworkFramesService,
 		private ui: UIService,
-	) {}
+	) { }
 
 	// Init the WebXR scene with Artworks
-	ngOnInit() {
+	ngOnInit () {
 		const start = Date.now();
 		this.artworks = this.artworksService.getArtworks();
-		const afterSceneInitCB = this.sceneService.initScene(this.canvasEl);
+		const afterSceneInitCB = this.sceneService.initScene( this.canvasEl );
 
 		// Model for Floor
-		const model = this.loadersService.loadModel({
+		const model = this.loadersService.loadModel( {
 			path: "assets/models/VRGallery0110NoFloorTexture.glb",
 			scene: this.sceneService.scene,
-			onLoadProgress: this.onLoadProgress.bind(this),
+			onLoadProgress: this.onLoadProgress.bind( this ),
 			onLoadCB: () => {
-				this.sceneService.addToScene(this.frames);
+				this.sceneService.addToScene( this.frames );
 				this.loadingBar = false;
 			},
-		});
+		} );
 
 		// Model for Walls
-		const modelWalls = this.loadersService.loadModel({
+		const modelWalls = this.loadersService.loadModel( {
 			path: "assets/models/VRGalleryInnerWalls0110Merge.glb",
 			scene: this.sceneService.scene,
-			onLoadProgress: this.onLoadProgress.bind(this),
+			onLoadProgress: this.onLoadProgress.bind( this ),
 			onLoadCB: () => {
-				afterSceneInitCB(start);
+				afterSceneInitCB( start );
 			},
-		});
+		} );
 
 		// Frames
 		// Buttons
@@ -84,8 +85,8 @@ export class GalleryComponent implements OnInit {
 			{
 				name: "Next Button",
 				text: "Next",
-				onClick: (ind: number) => {
-					this.changeSelection(ind, 1);
+				onClick: ( ind: number ) => {
+					this.changeSelection( ind, 1 );
 				},
 				position: { x: -0.75, y: 0, z: -0.0 },
 				rotation: {},
@@ -94,8 +95,8 @@ export class GalleryComponent implements OnInit {
 			{
 				name: "Upvote Button",
 				text: "Upvote",
-				onClick: (ind: number) => {
-					this.upvoteSelection(ind);
+				onClick: ( ind: number ) => {
+					this.upvoteSelection( ind );
 				},
 				position: { x: -0.8, y: 0.8, z: -0.1 },
 				rotation: {},
@@ -103,8 +104,8 @@ export class GalleryComponent implements OnInit {
 			{
 				name: "Previous Button",
 				text: "Previous",
-				onClick: (ind: number) => {
-					this.changeSelection(ind, -1);
+				onClick: ( ind: number ) => {
+					this.changeSelection( ind, -1 );
 				},
 				position: { x: 0.75, y: 0, z: -0 },
 				rotation: {},
@@ -118,54 +119,55 @@ export class GalleryComponent implements OnInit {
 		);
 
 		// UI
-		this.sceneService.renderFunctions.push(this.ui.update);
+		this.sceneService.renderFunctions.push( this.ui.update );
 
 		this.artworksLength = this.artworks.length;
 
-		this.selectedArtwork = signal(this.artworks[0], {
+		this.selectedArtwork = signal( this.artworks[0], {
 			equal: this.compareSelected,
-		});
+		} );
 	}
 
-	onLoadProgress(xhr: ProgressEvent) {
+	onLoadProgress ( xhr: ProgressEvent ) {
 		// console.log( 'OnProgress is running loaded ', xhr );
 		// console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	}
 
-	compareSelected(o: Artwork, n: Artwork) {
+	compareSelected ( o: Artwork, n: Artwork ) {
 		return o ? o.id === n.id : true;
 	}
 
 	// Handle Artwork selection events
-	onSelectArtwork() {
-		this.framesService.focusFrame(this.selectedArtwork().id);
+	onSelectArtwork () {
+		this.framesService.focusFrame( this.selectedArtwork().id );
 	}
 
 	/**
 	 * TODO: Select next artwork and show info panel?
-	 * @param e
+	 * @param ind number frame index for where the buttons are 
+	 * @param n: number direction, 1 for next, -1 for previous
 	 */
-	changeSelection(ind: any, n: number) {
-		console.log("Selection is changing", ind, n);
-		this.framesService.resetPosition(this.focused);
+	changeSelection ( ind: any, n: number ) {
+		this.framesService.resetPosition( this.focused );
 		let i;
-		if (n === 1) {
-			// Next
+		if ( n === 1 ) {
+			// Rotate to Next frame
 			i = ind < this.artworksLength - 1 ? ind + 1 : 0;
-			this.framesService.rotateFrames(72); //Rotate right
-		} else if (n === -1) {
-			// Previous
+			this.framesService.rotateFrames( 72 );
+		} else if ( n === -1 ) {
+			// Rotate to Previous
 			i = ind === 0 ? this.artworksLength - 1 : ind - 1;
-			this.framesService.rotateFrames(-72);
+			this.framesService.rotateFrames( -72 );
 		}
 		this.focused = i;
-		this.framesService.focusFrame(i);
+		this.framesService.focusFrame( i );
 	}
 
-	upvoteSelection(i: number): void {
-		this.artworksService.upvoteArtwork(i);
+	upvoteSelection ( i: number ): void {
+		this.artworksService.upvoteArtwork( i );
 		const name = `${i} Text`;
-		const textMesh = this.frames.children[i].getObjectByName(name);
-		this.ui.updateVote({ votes: this.artworks[i].votes, text: textMesh });
+		const textMesh = this.frames.children[i].getObjectByName( name );
+		this.ui.updateVote( { votes: this.artworks[i].votes, text: textMesh } );
 	}
+
 }
